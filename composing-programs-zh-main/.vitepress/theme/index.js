c073a99b3a2aac1dc75aa8f1dbc8f885adbc47cb
Shuu-39 <1397402891@qq.com>
2026-02-15 21:01:42 +0800

    完成了lab06

---
+++ composing-programs-zh-main/.vitepress/theme/index.js
@@ -0,0 +1,41 @@
+import { useData, useRoute } from 'vitepress'
+import giscusTalk from 'vitepress-plugin-comment-with-giscus'
+import DefaultTheme from 'vitepress/theme'
+import { h } from 'vue'
+import {
+  NolebaseEnhancedReadabilitiesMenu,
+  NolebaseEnhancedReadabilitiesScreenMenu,
+} from '@nolebase/vitepress-plugin-enhanced-readabilities/client'
+import '@nolebase/vitepress-plugin-enhanced-readabilities/client/style.css'
+import './styles.css'
+
+export default {
+  ...DefaultTheme,
+  Layout: () => {
+    return h(DefaultTheme.Layout, null, {
+      'nav-bar-content-after': () => h(NolebaseEnhancedReadabilitiesMenu),
+      'nav-screen-content-after': () => h(NolebaseEnhancedReadabilitiesScreenMenu),
+    })
+  },
+  enhanceApp(ctx) {
+    DefaultTheme.enhanceApp(ctx)
+  },
+  setup() {
+    const { frontmatter } = useData()
+    const route = useRoute()
+    giscusTalk(
+      {
+        repo: 'csfive/composing-programs-zh',
+        repoId: 'R_kgDOI7ryQw',
+        category: 'Announcements',
+        categoryId: 'DIC_kwDOI7ryQ84CWSPL',
+        mapping: 'title',
+        strict: '1',
+      },
+      {
+        frontmatter,
+        route,
+      },
+    )
+  },
+}
